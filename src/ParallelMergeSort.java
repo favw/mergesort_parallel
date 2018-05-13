5971c590f30d5509f898b9b0db5710eb67892955
favw <florian.wedelstedt@gmail.com>
2018-05-13 19:24:59 +0200

    Deleted Main.java, added main function.

--- src/ParallelMergeSort.java
+++ src/ParallelMergeSort.java
@@ -28,4 +28,24 @@ public class ParallelMergeSort {
                 }
         }
     }
+     public static void main(String[] args){
+        int n = 1000000;
+        int maxValue = Integer.MAX_VALUE;
+        Random rand = new Random();
+
+        int[] randomArray = new int[n]; //Array with 1.000.000 slots
+        for (int i = 0; i < n; i++) {
+            randomArray[i] = rand.nextInt(maxValue); //Filling array with random values
+        }
+
+
+        // 4 Threads
+        ParallelMergeSort.numberOfThreads = 4;
+        long timeStart = System.nanoTime(); //Time measurement
+        ParallelMergeSort.mergeSort(randomArray); //Initiates sorting
+        long timeEnd = System.nanoTime();
+        long timeDiff =  timeEnd - timeStart;
+
+        System.out.println("Parallel MergeSort with 4 Threads took: " + timeDiff + " nanoseconds.");
+    }
 }
